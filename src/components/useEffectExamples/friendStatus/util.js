4bededdb5980aad6644ba60c3f99fd2ed052ba1a
Shlomo Moller <moller.shlomo@gmail.com>
2021-11-09 21:20:10 +0200

    Create ChatAPI & a friend object

---
+++ src/components/useEffectExamples/friendStatus/util.js
@@ -0,0 +1,8 @@
+const ChatAPI = {
+    subscribeToFriendStatus: (friendId, onStatusChange) => {},
+    unsubscribeFromFriendStatus: (friendId, onStatusChange) => {},
+}
+
+const friend = { id: 123 }
+
+export {ChatAPI, friend}
