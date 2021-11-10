e1c4f9ff446f708a6ec799f40f7597b2c547f00c
Shlomo Moller <moller.shlomo@gmail.com>
2021-11-10 16:21:24 +0200

    Rename directories according to the guides
    Rename code example dirs to fit the Markdown guides they relate to

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import logo from './logo.svg'
 import './App.css'
-import HookRuleTest from './components/HooksWarningsExamples/HookRuleTest'
+import HookRuleTest from './components/React Hooks Examples/HookRuleTest'
 import Fieldset from './components/Fieldset'
-import LogHi from './components/useEffectExamples/LogHi'
-import Counter from './components/useEffectExamples/Counter'
-import Cleanup from './components/useEffectExamples/Cleanup'
-import FriendStatus from './components/useEffectExamples/friendStatus/FriendStatus'
-import { friend } from './components/useEffectExamples/friendStatus/util'
-import Lifecycle from './components/useEffectExamples/lifecycle/Lifecycle'
+import LogHi from './components/useEffect Examples/LogHi'
+import Counter from './components/useEffect Examples/Counter'
+import Cleanup from './components/useEffect Examples/Cleanup'
+import FriendStatus from './components/useEffect Examples/friendStatus/FriendStatus'
+import { friend } from './components/useEffect Examples/friendStatus/util'
+import Lifecycle from './components/useEffect Examples/lifecycle/Lifecycle'
 
 const App = () => {
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>My React App About React Hooks</p>
       </header>
+
       <div className='App-body'>
         <p>This is the body.</p>
+
+        {/* The "guides\React Hooks.md" Section */}
         <Fieldset legend='HookRuleTest'><HookRuleTest                 /></Fieldset>
+
+        {/* The "guides\useEffect.md" Section */}
         <Fieldset legend='LogHi'       ><LogHi                        /></Fieldset>
         <Fieldset legend='Counter'     ><Counter                      /></Fieldset>
         <Fieldset legend='Cleanup'     ><Cleanup                      /></Fieldset>
         <Fieldset legend='FriendStatus'><FriendStatus friend={friend} /></Fieldset>
         <Fieldset legend='Lifecycle'   ><Lifecycle                    /></Fieldset>
+
+        {/* The "guides\Custom Hooks.md" Section */}
+
       </div>
     </div>
   )
