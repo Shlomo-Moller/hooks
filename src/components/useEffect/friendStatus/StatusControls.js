9e05f8391d857cf51ce210b3908976424b8f6863
Shlomo Moller <moller.shlomo@gmail.com>
2021-11-10 18:39:21 +0200

    Create the StatusControls component

---
+++ src/components/useEffect/friendStatus/StatusControls.js
@@ -0,0 +1,11 @@
+const StatusControls = () => {
+    return (
+        <div className='status-controls'>
+            <label><input type='checkbox' id='friend-status-1' /> Connect friend 1</label>
+            <label><input type='checkbox' id='friend-status-2' /> Connect friend 2</label>
+            <label><input type='checkbox' id='friend-status-3' /> Connect friend 3</label>
+        </div>
+    )
+}
+
+export default StatusControls
