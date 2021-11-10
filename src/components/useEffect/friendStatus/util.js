5d07ee8be211ffb5f20aa56f8b601d46b1429b7e
Shlomo Moller <moller.shlomo@gmail.com>
2021-11-10 18:40:23 +0200

    Complete ChatAPI implementation & fix friend id

--- src/components/useEffect/friendStatus/util.js
+++ src/components/useEffect/friendStatus/util.js
@@ -1,8 +1,14 @@
 const ChatAPI = {
-    subscribeToFriendStatus: (friendId, onStatusChange) => {},
-    unsubscribeFromFriendStatus: (friendId, onStatusChange) => {},
+    subscribeToFriendStatus: (friendId, onStatusChange) => {
+        const checkbox = document.getElementById('friend-status-' + friendId)
+        checkbox?.addEventListener('change', () => onStatusChange({ isOnline: checkbox.checked }))
+    },
+    unsubscribeFromFriendStatus: (friendId, onStatusChange) => {
+        const checkbox = document.getElementById('friend-status-' + friendId)
+        checkbox?.removeEventListener('change', () => onStatusChange({ isOnline: checkbox.checked }))
+    },
 }
 
-const friend = { id: 123 }
+const friend = { id: 1 }
 
 export {ChatAPI, friend}
